8b8c1a8f1779370ba2241566b9d18466b4b1d967
Janneke van der Zwaan <j.vanderzwaan@esciencecenter.nl>
2017-03-29 22:48:08 +0200

    Fix where spaces are inserted in the gs text

--- README.md
+++ README.md
@@ -1,8 +1,20 @@
 
+## Plan
+
+* create ocr + gs for all files
+* align all ocr + gs files
+* quantify and qualify the differences between ocr and gs (what kind of ocr mistakes occur?)
+* dive into the neural networks
+
+## Ideas
+
+* Perhaps NN can be used to asses the quality of text? (character-based language model, the probability of generating a certain text)
+
 ## folia2ocr_and_gs
 
-* Not all files have ocr text (example: BNObi2-ds)
-  * This is a problem, because the separation of words by spaces depends on the presence of the ocroutput class
-* Not all words have ocr text (example: BNObi3-ds)
-  * This is a problem, because the separation of words by spaces depends on the presence of the ocroutput class
-* Additional space after ( (example: BNObi3-ds) (just like with " - is there a generic way to fix this?)
+* FIXED Not all files have ocr text (example: BNObi2-ds)
+  * FIXED This is a problem, because the separation of words by spaces depends on the presence of the ocroutput class
+* FIXED Not all words have ocr text (example: BNObi3-ds)
+  * FIXED This is a problem, because the separation of words by spaces depends on the presence of the ocroutput class
+  * Punctuation marks also do not have an ocrtext class
+* FIXED Additional space after ( (example: BNObi3-ds) (just like with " - is there a generic way to fix this?)
--- folia2ocr_and_gs.py
+++ folia2ocr_and_gs.py
@@ -12,12 +12,10 @@ from nlppln.utils import create_dirs
 def folia2ocr_and_gs(in_file, out_dir):
     create_dirs(out_dir)
 
-    quote = '{http://ilk.uvt.nl/folia}quote'
-
+    ocr_text_complete = []
     ocr_text = []
     gold_standard = []
-    ocr_word = None
-    prev_parent = None
+    punctuation = []
 
     # TODO: how to handle headings? They are sentences without a closing
     # punctuation mark.
@@ -26,38 +24,58 @@ def folia2ocr_and_gs(in_file, out_dir):
     context = etree.iterparse(in_file, tag='{http://ilk.uvt.nl/folia}w',
                               encoding='utf-8')
     for action, elem in context:
-        parent = elem.getparent().tag
+        ocr_word = ''
+        punc = False
+        pos_elem = elem.find('{http://ilk.uvt.nl/folia}pos')
+        if pos_elem is not None:
+            if pos_elem.get('class').startswith('LET'):
+                punc = True
         for t in elem.iterchildren(tag='{http://ilk.uvt.nl/folia}t'):
             if t.get('class') == 'ocroutput':
                 ocr_word = t.text
             else:
                 gs_word = t.text
 
-        if ocr_word:
-            space = True
+        if ocr_word != '':
             ocr_text.append(ocr_word)
-        # Add a space if the word is a " and it is the start of a quote.
-        # Otherwise, the " will be stuck to the previous word (which it should
-        # be if it is the end of the quote or any othe punctuation mark).
-        elif gs_word == '"' and parent == quote and prev_parent != quote:
-            space = True
-        else:
-            space = False
-
-        if space:
-            gold_standard.append(' ')
+        ocr_text_complete.append(ocr_word)
         gold_standard.append(gs_word)
-        ocr_word = None
-        space = False
-        prev_parent = parent
+        punctuation.append(punc)
 
-    # We need to normalize ' " ' in the gold standard to ' "', because we have
-    # introduced an additional space.
+    result = []
+    for i in range(len(ocr_text_complete)):
+        #print ocr_text_complete[i]
+        #print gold_standard[i]
+        result.append(gold_standard[i])
+        space = True
+        if punctuation[i]:
+            #print ocr_text_complete[i-1]
+            if i+1 < len(ocr_text_complete):
+                if ocr_text_complete[i+1].strip().startswith(gold_standard[i]):
+                    #print 'No space after this punctuation: {}'.format(gold_standard[i])
+                    space = False
+                if punctuation[i+1]:
+                    #print 'No space after this punctuation: '.format(gold_standard[i])
+                    space = False
+        elif i+1 < len(ocr_text_complete) and punctuation[i+1]:
+            #print '!'
+            if i+2 < len(ocr_text_complete):
+                if ocr_text_complete[i+2].strip().startswith(gold_standard[i+1]):
+                    #print 'Space after this word: {}'.format(gold_standard[i])
+                    space = True
+                else:
+                    #print 'No space after this word: {}'.format(gold_standard[i])
+                    space = False
+            else:
+                space = False
+        if space:
+            result.append(' ')
+
     fname = os.path.basename(in_file)
     fname = fname.replace('.folia.xml', '.{}.txt')
     fname = os.path.join(out_dir, fname)
     with codecs.open(fname.format('gs'), 'wb', encoding='utf-8') as f:
-        gs = u''.join(gold_standard).replace(u' " ', u' "').strip()
+        gs = u''.join(result).strip()
         f.write(gs)
 
     with codecs.open(fname.format('ocr'), 'wb', encoding='utf-8') as f:
