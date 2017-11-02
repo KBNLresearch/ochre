09ff4ce896ab7b302689e8d5e0b5a94d1beaaf92
Janneke van der Zwaan <j.vanderzwaan@esciencecenter.nl>
2017-11-02 14:38:56 +0100

    Clean up code

--- ochre/train_seq2seq.py
+++ ochre/train_seq2seq.py
@@ -13,7 +13,7 @@ import json
 @click.option('--weights_dir', '-w', default=os.getcwd(), type=click.Path())
 def train_lstm(datasets, data_dir, weights_dir):
     seq_length = 25
-    predict_chars = 1
+    pred_chars = 1
     num_nodes = 256
     layers = 2
     batch_size = 100
@@ -22,17 +22,17 @@ def train_lstm(datasets, data_dir, weights_dir):
     pad = u'\n'
 
     print('Sequence length: {}'.format(seq_length))
-    print('Predict characters: {}'.format(predict_chars))
+    print('Predict characters: {}'.format(pred_chars))
     print('Number of nodes in hidden layers: {}'.format(num_nodes))
     print('Number of hidden layers: {}'.format(layers))
     print('Batch size: {}'.format(batch_size))
     print('Lowercase data: {}'.format(lowercase))
 
-    division = json.load(datasets)
+    div = json.load(datasets)
 
-    raw_val, gs_val, ocr_val = read_texts(division.get('val'), data_dir)
-    raw_test, gs_test, ocr_test = read_texts(division.get('test'), data_dir)
-    raw_train, gs_train, ocr_train = read_texts(division.get('train'), data_dir)
+    raw_val, gs_val, ocr_val = read_texts(div.get('val'), data_dir)
+    raw_test, gs_test, ocr_test = read_texts(div.get('test'), data_dir)
+    raw_train, gs_train, ocr_train = read_texts(div.get('train'), data_dir)
 
     chars, n_vocab, char_to_int = get_chars(raw_val, raw_test, raw_train,
                                             lowercase, padding_char=pad)
@@ -41,23 +41,51 @@ def train_lstm(datasets, data_dir, weights_dir):
 
     print('Total Vocab: {}'.format(n_vocab))
 
-    numTrainSamples, trainDataGen = create_training_data(ocr_train, gs_train, char_to_int, n_vocab, seq_length=seq_length, batch_size=batch_size, lowercase=lowercase, step=step, predict_chars=predict_chars)
-    numTestSamples, testDataGen = create_training_data(ocr_test, gs_test, char_to_int, n_vocab, seq_length=seq_length, batch_size=batch_size, lowercase=lowercase, predict_chars=predict_chars)
-    numValSamples, valDataGen = create_training_data(ocr_val, gs_val, char_to_int, n_vocab, seq_length=seq_length, batch_size=batch_size, lowercase=lowercase, predict_chars=predict_chars)
+    nTrainSamples, trainData = create_training_data(ocr_train,
+                                                    gs_train,
+                                                    char_to_int,
+                                                    n_vocab,
+                                                    seq_length=seq_length,
+                                                    batch_size=batch_size,
+                                                    lowercase=lowercase,
+                                                    step=step,
+                                                    predict_chars=pred_chars)
+    nTestSamples, testData = create_training_data(ocr_test,
+                                                  gs_test,
+                                                  char_to_int,
+                                                  n_vocab,
+                                                  seq_length=seq_length,
+                                                  batch_size=batch_size,
+                                                  lowercase=lowercase,
+                                                  predict_chars=pred_chars)
+    nValSamples, valData = create_training_data(ocr_val,
+                                                gs_val,
+                                                char_to_int,
+                                                n_vocab,
+                                                seq_length=seq_length,
+                                                batch_size=batch_size,
+                                                lowercase=lowercase,
+                                                predict_chars=pred_chars)
 
-    n_patterns = numTrainSamples
+    n_patterns = nTrainSamples
     print("Train Patterns: {}".format(n_patterns))
-    print("Validation Patterns: {}".format(numValSamples))
-    print("Test Patterns: {}".format(numTestSamples))
-    print('Total: {}'.format(numTrainSamples+numTestSamples+numValSamples))
+    print("Validation Patterns: {}".format(nValSamples))
+    print("Test Patterns: {}".format(nTestSamples))
+    print('Total: {}'.format(nTrainSamples+nTestSamples+nValSamples))
 
-    model = initialize_model_seq2seq(num_nodes, 0.5, seq_length, predict_chars,
+    model = initialize_model_seq2seq(num_nodes, 0.5, seq_length, pred_chars,
                                      n_vocab, layers)
     epoch, model = load_weights(model, weights_dir)
     callbacks_list = [add_checkpoint(weights_dir)]
 
     # do training (and save weights)
-    model.fit_generator(trainDataGen, steps_per_epoch=int(numTrainSamples/batch_size), epochs=40, validation_data=valDataGen, validation_steps=int(numValSamples/batch_size), callbacks=callbacks_list, initial_epoch=epoch+1)
+    model.fit_generator(trainData,
+                        steps_per_epoch=int(nTrainSamples/batch_size),
+                        epochs=40,
+                        validation_data=valData,
+                        validation_steps=int(nValSamples/batch_size),
+                        callbacks=callbacks_list,
+                        initial_epoch=epoch+1)
 
 
 if __name__ == '__main__':
