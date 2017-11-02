6bc0ac5140164a99710b36a5b946f4b2cd5e0edd
Janneke van der Zwaan <j.vanderzwaan@esciencecenter.nl>
2017-11-02 14:30:54 +0100

    Fix initial epoch

--- ochre/train_seq2seq.py
+++ ochre/train_seq2seq.py
@@ -57,7 +57,7 @@ def train_lstm(datasets, data_dir, weights_dir):
     callbacks_list = [add_checkpoint(weights_dir)]
 
     # do training (and save weights)
-    model.fit_generator(trainDataGen, steps_per_epoch=int(numTrainSamples/batch_size), epochs=40, validation_data=valDataGen, validation_steps=int(numValSamples/batch_size), callbacks=callbacks_list, initial_epoch=epoch)
+    model.fit_generator(trainDataGen, steps_per_epoch=int(numTrainSamples/batch_size), epochs=40, validation_data=valDataGen, validation_steps=int(numValSamples/batch_size), callbacks=callbacks_list, initial_epoch=epoch+1)
 
 
 if __name__ == '__main__':
