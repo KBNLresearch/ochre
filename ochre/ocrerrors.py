1cc310272957b41be4d4e679b701dec09619d32f
Janneke van der Zwaan <j.vanderzwaan@esciencecenter.nl>
2017-12-18 13:06:16 +0100

    Add new error types and an api to get the error types
    New error types:
    
    - missing_puncuation_mark_error
    - missing_word_error
    - real_word_error (uses word list)

--- ochre/ocrerrors.py
+++ ochre/ocrerrors.py
@@ -2,16 +2,33 @@
 # -*- coding: utf-8 -*-
 import unicodedata
 
+from collections import OrderedDict
 from string import punctuation
 
 
-def num_errors(df, error_function):
-    s = df[df.apply(error_function, axis=1)].shape
+def get_error_types():
+    error_types = OrderedDict()
+    error_types['hyphenation'] = hyphenation_error
+    error_types['hyphenation2'] = hyphenation_error2
+    error_types['case'] = case_error
+    error_types['punctuation'] = punctuation_mark_error
+    error_types['missing_punctuation'] = missing_punctuation_mark_error
+    error_types['missing_word'] = missing_word_error
+    error_types['number'] = number_error
+    error_types['whitespace'] = whitespace_error
+    error_types['accent'] = accent_error
+    error_types['real_word'] = real_word_error
+
+    return error_types
+
+
+def num_errors(df, error_function, args=None):
+    s = df[df.apply(error_function, args=args, axis=1)].shape
     return s[0]
 
 
-def remove_errors(df, error_function):
-    return df[df.apply(error_function, axis=1) == False]
+def remove_errors(df, error_function, args=None):
+    return df[df.apply(error_function, args=args, axis=1) == False]
 
 
 def hyphenation_error(row):
@@ -61,6 +78,20 @@ def punctuation_mark_error(row):
     return False
 
 
+def missing_punctuation_mark_error(row):
+    #ocr = ''
+    #gs = ','
+    gs = row['gs']
+    ocr = row['ocr']
+
+    #print type(ocr)
+    #print type(gs)
+
+    if gs in punctuation and ocr == u'@@@':
+        return True
+    return False
+
+
 def number_error(row):
     #ocr = '64-jarige'
     #gs = '-jarig'
@@ -90,7 +121,30 @@ def accent_error(row):
     gs = row['gs']
     ocr = row['ocr']
 
-    no_accents = ''.join((c for c in unicodedata.normalize('NFD', gs) if unicodedata.category(c) != 'Mn'))
+    no_accents = u''.join((c for c in unicodedata.normalize('NFD', unicode(gs)) if unicodedata.category(c) != 'Mn'))
     if no_accents == ocr:
         return True
     return False
+
+
+def missing_word_error(row):
+    gs = row['gs']
+    ocr = row['ocr']
+
+    #print type(ocr)
+    #print type(gs)
+
+    if gs not in punctuation and ocr == u'@@@':
+        #print gs
+        return True
+    return False
+
+
+def real_word_error(row, terms):
+    #gs = u'hallo'
+    #ocr = u'boom'
+
+    gs = row['gs']
+    ocr = row['ocr']
+
+    return ocr in terms
