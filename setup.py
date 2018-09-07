71948365487ec96761c7b6a53a44a6d2b7f97adf
Janneke van der Zwaan <j.vanderzwaan@esciencecenter.nl>
2018-09-07 19:34:58 +0200

    Update requirement nlppln version

--- setup.py
+++ setup.py
@@ -61,7 +61,7 @@ setup(
     # your project is installed. For an analysis of "install_requires" vs pip's
     # requirements files see:
     # https://packaging.python.org/en/latest/requirements.html
-    install_requires=['nlppln']
+    install_requires=['nlppln>=0.3.2']
 
     #scripts=['recipy-cmd']
 )
