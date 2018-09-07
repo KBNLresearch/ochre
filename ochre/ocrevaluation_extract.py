9d7f9fac19b148a8d96da2c7a1c370be81f097d9
Janneke van der Zwaan <j.vanderzwaan@esciencecenter.nl>
2018-09-07 19:48:25 +0200

    Deal with large input files
    When the texts that are compared are big, the ocrevaluation result can
    become to big to analyze conveniently. This is dealt with by ignoring the
    the character differences, because we are not interested in them anyway.

--- ochre/ocrevaluation_extract.py
+++ ochre/ocrevaluation_extract.py
@@ -2,6 +2,7 @@
 import click
 import codecs
 import os
+import tempfile
 
 from bs4 import BeautifulSoup
 
@@ -9,18 +10,37 @@ from nlppln.utils import create_dirs, remove_ext
 
 
 @click.command()
-@click.argument('in_file', type=click.File())
+@click.argument('in_file', type=click.File(encoding='utf-8'))
 @click.option('--out_dir', '-o', default=os.getcwd(), type=click.Path())
 def ocrevaluation_extract(in_file, out_dir):
     create_dirs(out_dir)
 
-    soup = BeautifulSoup(in_file, 'lxml')
+    tables = []
+
+    write = False
+
+    (fd, tmpfile) = tempfile.mkstemp()
+    with codecs.open(tmpfile, 'w', encoding='utf-8') as tmp:
+        for line in in_file:
+            if line.startswith('<h2>General'):
+                write = True
+            if line.startswith('<h2>Difference'):
+                write = False
+            if line.startswith('<h2>Error'):
+                write = True
+
+            if write:
+                tmp.write(line)
+
+    with codecs.open(tmpfile, encoding='utf-8') as f:
+        soup = BeautifulSoup(f.read(), 'lxml')
+
     tables = soup.find_all('table')
-    assert len(tables) == 3
+    assert len(tables) == 2
+    os.remove(tmpfile)
 
     doc = remove_ext(in_file.name)
 
-    # global measures: table[0]
     t = tables[0]
     table_data = [[cell.text for cell in row('td')] for row in t('tr')]
 
@@ -42,8 +62,7 @@ def ocrevaluation_extract(in_file, out_dir):
             f.write(u','.join(lines[i]))
             f.write(u'\n')
 
-    # character measures: table[2]
-    t = tables[2]
+    t = tables[1]
     table_data = [[cell.text for cell in row('td')] for row in t('tr')]
     out_file = os.path.join(out_dir, '{}-character.csv'.format(doc))
     with codecs.open(out_file, 'wb', encoding='utf-8') as f:
