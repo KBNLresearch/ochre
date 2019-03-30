3ee0c4588555b0213267a604d41f2a1fd1128587
Janneke van der Zwaan <j.vanderzwaan@esciencecenter.nl>
2019-03-30 11:09:10 +0100

    Load modules and set shorter time for testing

--- job.sh
+++ job.sh
@@ -5,7 +5,14 @@
 #
 #SBATCH --ntasks=1
 #SBATCH -C TitanX
-#SBATCH --time=15:00:00
+#SBATCH --time=:01:00
 #SBATCH --gres=gpu:1
+
+module load python/3.5.2
+module load python-extra/python3.5/r0.5.0
+module load cuda80/toolkit/8.0.61
+module load tensorflow/python3.x/gpu/r1.4.0-py3
+module load keras/python3.5/r2.0.2
+module load cuDNN/cuda80/5.1.5
 
 srun python /home/jvdzwaan/code/ochre/2017_baseline.py
