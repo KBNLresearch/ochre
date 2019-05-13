ae2c5550e7da16eec84c1c20172a27ad9179ef31
Janneke van der Zwaan <j.vanderzwaan@esciencecenter.nl>
2019-05-13 15:57:31 +0200

    Use fixed values instead of variables

--- nematus_translate.sh
+++ nematus_translate.sh
@@ -7,10 +7,6 @@
 #SBATCH -C TitanX
 #SBATCH --gres=gpu:1
 
-inputdir = /var/scratch/jvdzwaan/kb-ocr/sample-nematus-text_aligned_blocks-match_gs/sample-test/
-modelfile = /var/scratch/jvdzwaan/kb-ocr/sample-nematus-text_aligned_blocks-match_gs/model/model-110000
-outputdir = /var/scratch/jvdzwaan/kb-ocr/sample-nematus-text_aligned_blocks-match_gs/pred
-
-for filename in "$inputdir"/*.ocr; do
-  echo	srun python ~/code/nematus/nematus/translate.py -m "$modelfile" -i "$filename" -o "$outputdir/$(basename "$filename" .ocr).pred"
+for filename in /var/scratch/jvdzwaan/kb-ocr/sample-nematus-text_aligned_blocks-match_gs/sample-test/*.ocr; do
+  echo python ~/code/nematus/nematus/translate.py -m /var/scratch/jvdzwaan/kb-ocr/sample-nematus-text_aligned_blocks-match_gs/model/model-110000 -i "$filename" -o /var/scratch/jvdzwaan/kb-ocr/sample-nematus-text_aligned_blocks-match_gs/pred/"$(basename "$filename" .ocr).pred"
 done
