dae34c50613450e9d1a7e75895c5522d55284678
Janneke van der Zwaan <j.vanderzwaan@esciencecenter.nl>
2019-05-13 16:49:38 +0200

    Run for all test files (not just the sample)

--- nematus_translate.sh
+++ nematus_translate.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 #
 #SBATCH --job-name=nematus_translate
-#SBATCH --output=nematus_translate_eng-mon_context.txt
+#SBATCH --output=nematus_translate_kb-ocr-sample.txt
 #
 #SBATCH --ntasks=1
 #SBATCH -C TitanX
 #SBATCH --gres=gpu:1
 
-for filename in /var/scratch/jvdzwaan/kb-ocr/sample-nematus-text_aligned_blocks-match_gs/sample-test/*.ocr; do
-  echo python ~/code/nematus/nematus/translate.py -m /var/scratch/jvdzwaan/kb-ocr/sample-nematus-text_aligned_blocks-match_gs/model/model-110000 -i "$filename" -o /var/scratch/jvdzwaan/kb-ocr/sample-nematus-text_aligned_blocks-match_gs/pred/"$(basename "$filename" .ocr).pred"
+for filename in /var/scratch/jvdzwaan/kb-ocr/sample-nematus-text_aligned_blocks-match_gs/test/*.ocr; do
+  python ~/code/nematus/nematus/translate.py -m /var/scratch/jvdzwaan/kb-ocr/sample-nematus-text_aligned_blocks-match_gs/model/model-110000 -i "$filename" -o /var/scratch/jvdzwaan/kb-ocr/sample-nematus-text_aligned_blocks-match_gs/pred/"$(basename "$filename" .ocr).pred"
 done
