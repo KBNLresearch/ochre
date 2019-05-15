1f776ec7ba0a70aee295ac259b95e633ed64be33
Janneke van der Zwaan <j.vanderzwaan@esciencecenter.nl>
2019-05-15 15:53:37 +0200

    Update job scripts

--- nematus.sh
+++ nematus.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 #
-#SBATCH --job-name=nematus_eng-mon_context
-#SBATCH --output=nematus_eng-mon_context.txt
+#SBATCH --job-name=nematus_kb-ocr
+#SBATCH --output=nematus_kb-ocr.txt
 #
 #SBATCH --ntasks=1
 #SBATCH -C TitanX
-#SBATCH --time=00:00:00
 #SBATCH --begin=20:00
+#SBATCH --time=12:00:00
 #SBATCH --gres=gpu:1
 
-srun mkdir -p /var/scratch/jvdzwaan/nematus/model/
-srun python ~/code/nematus/nematus/train.py --source_dataset ~/data/nematus/train-eng_mon-context.ocr --target_dataset ~/data/nematus/train-eng_mon-context.gs --embedding_size 256 --tie_encoder_decoder_embeddings --rnn_use_dropout --batch_size 100 --valid_source_dataset ~/data/nematus/train-eng_mon-context.gs --valid_target_dataset ~/data/nematus/val-eng_mon-context.gs --dictionaries ~/data/nematus/train.gs.json ~/data/nematus/train.gs.json --valid_batch_size 100 --model /var/scratch/jvdzwaan/nematus/model/model
+srun mkdir -p /var/scratch/jvdzwaan/kb-ocr/A8P1/model/
+srun python ~/code/nematus/nematus/train.py --source_dataset /var/scratch/jvdzwaan/kb-ocr/A8P1/train.ocr --target_dataset /var/scratch/jvdzwaan/kb-ocr/A8P1/train.gs --embedding_size 256 --tie_encoder_decoder_embeddings --rnn_use_dropout --batch_size 100 --valid_source_dataset /var/scratch/jvdzwaan/kb-ocr/A8P1/val.ocr --valid_target_dataset /var/scratch/jvdzwaan/kb-ocr/A8P1/val.gs --dictionaries /var/scratch/jvdzwaan/kb-ocr/A8P1/train.json /var/scratch/jvdzwaan/kb-ocr/A8P1/train.json --valid_batch_size 100 --model /var/scratch/jvdzwaan/kb-ocr/A8P1/model/model --reload latest_checkpoint --save_freq 10000
