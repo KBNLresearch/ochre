0c42eef07829e458297626afe7d16d0cd7ce7bb1
Janneke van der Zwaan <j.vanderzwaan@esciencecenter.nl>
2019-09-16 19:18:01 +0200

    Update package url

--- setup.py
+++ setup.py
@@ -19,7 +19,7 @@ setup(
 """,
 
     # The project's main homepage.
-    url='https://github.com/WhatWorksWhenForWhom/nlppln',
+    url='https://github.com/KBNLresearch/ochre',
 
     # Author details
     author='Janneke van der Zwaan',
