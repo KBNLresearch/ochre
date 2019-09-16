3bf1f3776dd0483b23a4e15285a467519c05449a
Janneke van der Zwaan <j.vanderzwaan@esciencecenter.nl>
2019-09-16 19:58:07 +0200

    Set up commands for testing and building documentation

--- setup.py
+++ setup.py
@@ -68,6 +68,19 @@ setup(
         'six',
         'glob2',
         'sh',
+    ],
+    setup_requires=[
+        # dependency for `python setup.py test`
+        'pytest-runner',
+        # dependencies for `python setup.py build_sphinx`
+        'sphinx',
+        'sphinx_rtd_theme',
+        'recommonmark'
+    ],
+    tests_require=[
+        'pytest',
+        'pytest-cov',
+        'pycodestyle',
     ]
 
     #scripts=['recipy-cmd']
